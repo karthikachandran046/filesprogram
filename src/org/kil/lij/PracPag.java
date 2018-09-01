1bb202fdcd6fcfc4f22263042eb3ab999b185f93
karthikachandran046 <karthikachandran046>
2018-09-01 09:34:30 +0530

    some changes

--- src/org/kil/lij/PracPag.java
+++ src/org/kil/lij/PracPag.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class PracPag {
 	public static void main(String[] args) {
-		
-
-@SuppressWarnings("resource")
 Scanner rev=new Scanner(System.in);
-System.out.println("enter the num");
+System.out.println("enter the number");
 int n1=rev.nextInt();
 int a,i=0,j=0;
 a=n1;
@@ -17,8 +14,7 @@ while(a>0) {
 	j=(j*10)+i;		
     a=a/10;
 }
-System.out.println("reverse num");
+System.out.println("reverse number");
 }
-
 }
 
